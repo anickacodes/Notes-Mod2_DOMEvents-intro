2d0035fb0c81ce05a8f1f9902e3aee4efd71f80a
anickacodes <anickalewis@pursuit.org>
2023-06-22 11:06:36 -0400

    notes from our events- intro

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("shawwty");
+//DOM = Document Object Model
+const h1 = document.querySelector("h1");
+//calling the argument on the object and receives a string
+
+console.log("does it work?", h1)
+
+h1.textContent="why my background-color being a garden tool?"
